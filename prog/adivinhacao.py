855ce310418a0790d556c1d03f148b33de2dcb4f
thiagosagara <thiagosagara@gmail.com>
2020-05-06 18:07:46 -0300

    Troca do while pelo for

--- prog/adivinhacao.py
+++ prog/adivinhacao.py
@@ -3,9 +3,8 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
 
-while (rodada <= total_de_tentativas):
+for rodada in range(1,total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
 
     chute_str = input("Digite o seu número: ")
@@ -23,7 +22,5 @@ while (rodada <= total_de_tentativas):
             print("O seu chute foi maior do que o número secreto!")
         elif(menor):
             print("O seu chute foi menor do que o número secreto!")
-
-    rodada = rodada + 1
 
 print("Fim do jogo")
