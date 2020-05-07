12204bc71a262f4bf6032b17e497cd0cba7b65f8
thiagosagara <thiagosagara@gmail.com>
2020-05-07 11:57:22 -0300

    Inclusão do nivel

--- prog/adivinhacao.py
+++ prog/adivinhacao.py
@@ -10,6 +10,18 @@ print("*********************************")
 numero_secreto = random.randrange(1,101)
 total_de_tentativas = 3
 
+print("Qual o nível de dificuldade?")
+print("(1) Fácil | (2) Médio | (3) Difícil")
+
+nivel = int(input("Define o nível: "))
+
+if(nivel == 1):
+    total_de_tentativas = 10
+elif(nivel == 2):
+    total_de_tentativas = 5
+else:
+    total_de_tentativas = 3
+
 for rodada in range(1,total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
 
