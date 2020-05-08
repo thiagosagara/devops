b31070e85cda96404482fcbf26a189c1dd334ba4
thiagosagara <thiagosagara@gmail.com>
2020-05-08 14:08:34 -0300

    Adicionado as funções, trabalhando com return

--- prog/forca.py
+++ prog/forca.py
@@ -1,61 +1,21 @@
-
 import random
 
 def jogar():
-    print("*********************************")
-    print("***Bem vindo ao jogo da Forca!***")
-    print("*********************************")
 
-    #adicionado o upper na palabra secreta
-    #palavra_secreta = "one".upper()
-    #usando o list comprehension
-    #letras_acertadas = ["_" for letra in palavra_secreta]
-
-    #outro jeito é fazer do modo tradicional
-    #for letra in palavra_secreta:
-    #    letras_acertadas.append("_")
-
-    arquivo = open("palavras.txt", "r")
-    palavras = []
-
-    for linha in arquivo:
-        linha = linha.strip()
-        #adiciona tudo no txt para uma lista
-        palavras.append(linha)
-
-    arquivo.close()
-
-    # com base no tamano da lista (len(palavras)), ele escolhe randomicamente a palavra
-    numero = random.randrange(0, len(palavras))
-
-    #com base no numero (index), ele cria a instancia ja com a letra em maiuscula
-    palavra_secreta = palavras[numero].upper()
-    letras_acertadas = ["_" for letra in palavra_secreta]
-
+    imprime_boas_vindas()
+    palavra_secreta = carrega_palavra_secreta()
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
 
     #usando a variavel do tipo bool
     enforcou = False
     acertou = False
     erros = 0
-
     print(letras_acertadas)
 
     while (not acertou and not enforcou):
-
-        chute = input("Digite a letra: ")
-        #o strip remove os espaços em branco e caracteres especiais (como o \n)
-        #alteramos o chute do usuario para tudo em maiusculas
-        chute = chute.strip().upper()
-
+        chute = pede_chute()
         if (chute in palavra_secreta):
-            index = 0
-            for letra in palavra_secreta:
-                #upper serve para coloca todas as letras em maiuscula
-                if(chute == letra):
-                    # Nessa proxima linha ele vai substituir a letra (ou o chute (vai dar na mesma))
-                    # pelo "_" correspondente
-                    letras_acertadas[index] = letra
-                index += 1
+            marca_chute_correto(chute,letras_acertadas,palavra_secreta)
         else:
             erros += 1
             print("Ops, você errou! Faltam {} tentativas.".format(6 - erros))
@@ -65,9 +25,95 @@ def jogar():
         print(letras_acertadas)
 
     if (acertou):
-        print("Você ganhou!")
+        imprime_msg_vencedor()
     else:
-        print("Você perdeu!")
+        imprime_msg_perdedor(palavra_secreta)
+
+def imprime_boas_vindas():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+def carrega_palavra_secreta():
+    # adicionado o upper na palabra secreta
+    # palavra_secreta = "one".upper()
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        # adiciona tudo no txt para uma lista
+        palavras.append(linha)
+
+    arquivo.close()
+
+    # com base no tamano da lista (len(palavras)), ele escolhe randomicamente a palavra
+    numero = random.randrange(0, len(palavras))
+
+    # com base no numero (index), ele cria a instancia ja com a letra em maiuscula
+    palavra_secreta = palavras[numero].upper()
+    return palavra_secreta
+
+def inicializa_letras_acertadas(palavra):
+    # usando o list comprehension
+    # letras_acertadas = ["_" for letra in palavra_secreta]
+    # outro jeito é fazer do modo tradicional
+    # for letra in palavra_secreta:
+    #    letras_acertadas.append("_")
+
+    return ["_" for letra in palavra]
+
+def pede_chute():
+    chute = input("Digite a letra: ")
+    # o strip remove os espaços em branco e caracteres especiais (como o \n)
+    # alteramos o chute do usuario para tudo em maiusculas
+    chute = chute.strip().upper()
+    return chute
+
+def marca_chute_correto(chute,letras_acertadas,palavra_secreta):
+    index = 0
+    for letra in palavra_secreta:
+        # upper serve para coloca todas as letras em maiuscula
+        if (chute == letra):
+            # Nessa proxima linha ele vai substituir a letra (ou o chute (vai dar na mesma))
+            # pelo "_" correspondente
+            letras_acertadas[index] = letra
+        index += 1
+    return
+
+def imprime_msg_vencedor():
+    print("Parabéns, você ganhou!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+
+def imprime_msg_perdedor(palavra_secreta):
+    print("Puxa, você foi enforcado!")
+    print("A palavra era {}".format(palavra_secreta))
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
+
 
 if (__name__ == "__main__"):
     jogar()
